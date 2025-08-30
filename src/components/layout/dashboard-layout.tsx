ea1ddc9756041e658d4a4c269247f6903fabc6c2
Philip Onyango <onyangophilip244@gmail.com>
2025-08-30 03:39:13 +0000

    make the entire screen be static be fixed on all devicess so make sure t

--- src/components/layout/dashboard-layout.tsx
+++ src/components/layout/dashboard-layout.tsx
@@ -7,9 +7,9 @@ export function DashboardLayout({ children }: { children: ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <SidebarInset>
+      <SidebarInset className="flex h-screen flex-col">
         <AppHeader />
-        <main className="min-h-[calc(100vh-4rem)] p-2 sm:p-4 lg:p-6">
+        <main className="flex-1 overflow-y-auto p-2 sm:p-4 lg:p-6">
           {children}
         </main>
       </SidebarInset>
